7962640b24ec938526465bca7995074b11ed0203
SalmanIbnaKabir <salmanibnakabir@gmail.com>
2022-11-18 14:47:07 +0600

    review api add

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
   try {
     const serviceCollection = client.db('assignmentDb_11').collection('services');
+    const reviewsCollection = client.db('assignmentDb_11').collection('reviews');
 
     app.get('/services', async (req, res) => {
       const query = {};
@@ -48,6 +49,13 @@ async function run() {
       const query = { _id: ObjectId(id) };
       const service = await serviceCollection.findOne(query);
       res.send(service);
+    })
+
+    app.post('/reviews', async (req, res) => {
+      const review = req.body;
+      // console.log(review);
+      const result = await reviewsCollection.insertOne(review);
+      res.send(result)
     })
 
 
